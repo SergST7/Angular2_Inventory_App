df0185dcc12e8a3fd090283b129e03af4a36f541
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-02-16 22:53:56 +0200

    added bootsrap module

--- app.ts
+++ app.ts
@@ -1,2 +1,26 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+import {
+    Component,
+} from '@angular/core';
+
+@Component({
+
+})
+class InventoryApp {
+  // Inventory logic here
+}
 
 
+@NgModule({
+  declarations: [
+    InventoryApp
+  ],
+  imports: [ BrowserModule ],
+  bootstrap: [ InventoryApp ]
+})
+class InventoryAppModule {}
+
+platformBrowserDynamic().bootstrapModule(InventoryAppModule);
